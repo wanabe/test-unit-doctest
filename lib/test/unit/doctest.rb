8438f905da2718bcb453b83ca237ffb82f8a61be
wanabe <s.wanabe@gmail.com>
2015-07-06 07:43:56 +0900

    Add Test::Unit::Doctest.generate

--- lib/test/unit/doctest.rb
+++ lib/test/unit/doctest.rb
@@ -1,11 +1,15 @@
 require "test/unit/doctest/parser"
 require "test/unit/doctest/runner"
+require "test/unit/doctest/generator"
 require "test/unit/doctest/version"
 
 module Test::Unit
   module Doctest
     def self.run(files, require_file=true)
       Array(files).each { |file| Runner.new(file).run(require_file) }
+    end
+    def self.generate(file_class_table)
+      file_class_table.each { |file, class_name| Generator.new(file, class_name).generate }
     end
   end
 end
