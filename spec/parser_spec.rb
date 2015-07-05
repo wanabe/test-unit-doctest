117163aa6abf8bab42585f2480f391e72266e5be
wanabe <s.wanabe@gmail.com>
2015-07-06 07:18:29 +0900

    Fix spec to use test-unit

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,29 +1,29 @@
 require_relative "spec_helper"
 
-describe Minitest::Doctest::Parser do
-  before do
-    @parser = Minitest::Doctest::Parser.new(SOURCE)
+class Test::Unit::Doctest::ParserTest < Test::Unit::TestCase
+  setup do
+    @parser = Test::Unit::Doctest::Parser.new(SOURCE)
     @blocks = @parser.blocks
   end
 
-  it "should find the doctest comment blocks" do
-    @blocks.size.must_equal 2
-    @blocks[0].lines.size.must_equal 4
-    @blocks[1].lines.size.must_equal 2
+  test "find the doctest comment blocks" do
+    assert_equal 2, @blocks.size
+    assert_equal 4, @blocks[0].lines.size
+    assert_equal 2, @blocks[1].lines.size
   end
 
-  it "should create expression and assertion lines from blocks" do
+  test "create expression and assertion lines from blocks" do
     lines = @blocks.map { |block| @parser.test_lines(block) }
-    lines[0][0].must_equal "_ = Calculator.add(1, 2)"
-    lines[0][1].must_equal "assert_equal(3, _)"
-    lines[0][2].must_equal "_ = Calculator.add(1, 10, 100, 1000)"
-    lines[0][3].must_equal "assert_equal(1111, _)"
-    lines[1][0].must_equal "_ = Calculator.add(:a, 42)"
-    lines[1][1].must_equal "assert_equal(nil, _)"
+    assert_equal "_ = Calculator.add(1, 2)", lines[0][0]
+    assert_equal "assert_equal(3, _)", lines[0][1]
+    assert_equal "_ = Calculator.add(1, 10, 100, 1000)", lines[0][2]
+    assert_equal "assert_equal(1111, _)", lines[0][3]
+    assert_equal "_ = Calculator.add(:a, 42)", lines[1][0]
+    assert_equal "assert_equal(nil, _)", lines[1][1]
   end
 
-  it "should create test string from lines" do
+  test "create test string from lines" do
     test = @parser.test_string(@parser.test_lines(@blocks[1]), 1)
-    test.must_match /\s*def test_1$\s*_ = Calculator.add\(:a, 42\)$\s*assert_equal\(nil, _\)$\s*end/
+    assert_match /\s*def test_1$\s*_ = Calculator.add\(:a, 42\)$\s*assert_equal\(nil, _\)$\s*end/, test
   end
 end
--- spec/runner_spec.rb
+++ spec/runner_spec.rb
@@ -1,18 +1,18 @@
 require_relative "spec_helper"
 
-describe Minitest::Doctest::Runner do
-  before do
-    @runner = Minitest::Doctest::Runner.new(FILE)
+class Test::Unit::Doctest::RunnerTest < Test::Unit::TestCase
+  setup do
+    @runner = Test::Unit::Doctest::Runner.new(FILE)
   end
 
-  it "should define test methods to test class" do
+  test "define test methods to test class" do
     @runner.define_tests
     test = @runner.test_class.new("test")
-    test.must_respond_to :test_0
-    test.must_respond_to :test_1
+    assert_respond_to test, :test_0
+    assert_respond_to test, :test_1
   end
 
-  it "should return file name with #to_s on test_class" do
-    @runner.test_class.to_s.must_match "calculator.rb"
+  test "return file name with #to_s on test_class" do
+    assert_match "calculator.rb", @runner.test_class.to_s
   end
 end
